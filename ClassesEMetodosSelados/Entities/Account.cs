051064b3d4bdbdbef5ad3dbde0d4334283ac5ceb
Jose Juare <juarez.junior@outlook.com>
2021-04-11 11:20:57 -0300

    Adding ClassesEMetodosSelados project

---
+++ ClassesEMetodosSelados/Entities/Account.cs
@@ -0,0 +1,36 @@
+﻿namespace ClassesEMetodosSelados.Entities
+{
+    class Account
+    {
+        //Attibutes
+        public int Number { get; private set; }//Número da conta
+        public string Holder { get; private set; }//Titular da conta
+        public double Balance { get; protected set; }//Saldo - O modificador de acesso "protected", indica que  esse dado somente será acessado na classe ou em sub-classes!
+
+        //Constructors
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        //Methods
+        /*A palavra "virtual" entre "public" e "void", permite a sobrescrição
+        **ou sobreposição do método nas subclasses!
+        */
+        public virtual void Withdraw(double amount)//Saque
+        {
+            Balance -= amount + 5.0;//Na superclasse, o saque terá uma taxa de $5.00
+        }
+
+        public void Deposit(double amount)//Depósito
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ ClassesEMetodosSelados/Entities/SavingsAccount.cs
@@ -0,0 +1,43 @@
+﻿namespace ClassesEMetodosSelados.Entities
+{
+    /* A termo "sealed" antes de "class" indica que essa é uma classe
+     * que não poderá ser herdada.
+    */
+    sealed class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        /*A palavra "override" aqui, indica que o método da subclasse "Withdraw"
+        **está sendo sobrescrevido aqui. Ou seja, o método será diferente.
+        */
+        public override void Withdraw(double amount)
+        /* Caso fosse declarado dessa forma:
+         * public sealed override void Withdraw(double amount)
+         * esse método impede que o método "Withdraw" seja
+         * sobrescrito em outra .
+        */
+        {
+            /*Nesse caso, o uso da palavra "base", utiliza o método da superclasse
+            ** e ainda é possível implementar mais operação.
+            **Portanto aqui será descontado os $5.00 da superclasse(Account)
+            **e mais $2.00. Totalizando assim $7.0.
+            */
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
