ba3ac9dc0b087008638cb3417719520c9da56be8
Jose Juarez <juarez.junior@outlook.com>
2021-04-11 19:26:40 -0300

    Adding ClassesEMetodosAbstratos project

---
+++ ClassesEMetodosAbstratos/Entities/Company.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassesEMetodosAbstratos.Entities
+{
+    class Company : TaxPayer
+    {
+        public int NumberOfEmployees { get; set; }
+
+        public Company()
+        {
+        }
+
+        public Company(string name, double anualIncome, int numberOfEmployees)
+            : base(name, anualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+        public override double Tax()
+        {
+            if (NumberOfEmployees <= 10)
+            {
+                return AnualIncome * 0.16;
+            }
+            else
+            {
+                return AnualIncome * 0.14;
+            }
+        }
+    }
+}
---
+++ ClassesEMetodosAbstratos/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using ClassesEMetodosAbstratos.Entities;
+
+namespace ClassesEMetodosAbstratos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Aula 135 e Exercício aula 137
+
+            List<TaxPayer> list = new List<TaxPayer>();
+
+            Console.Write("Enter the number of tax payers: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Tax payer #{i} data:");
+                Console.Write("Individual or company (i/c)? ");
+                char ch = char.Parse(Console.ReadLine());
+                if (ch == 'i' || ch == 'I')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Anual income: ");
+                    double anualIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Health expenditures: ");
+                    double healthExpenditures = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Individual(name, anualIncome, healthExpenditures));
+                }
+                else if (ch == 'c' || ch == 'C')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Anual income: ");
+                    double anualIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Number of employees: ");
+                    int numberOfEmployees = int.Parse(Console.ReadLine());
+                    list.Add(new Company(name, anualIncome, numberOfEmployees));
+                }
+                else
+                {
+                    Console.WriteLine("Unknown error! Contact administrator.");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("TAXES PAID:");
+            double sum = 0.0;
+            foreach (TaxPayer taxPayer in list)
+            {
+                double tax = taxPayer.Tax();
+                sum += tax;
+                Console.WriteLine(taxPayer.Name + ": $ " + tax.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("TOTAL TAXES: $ " + sum.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
