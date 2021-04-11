45497012a68d01d31d187451c0e61fe11499b81e
Jose Juare <juarez.junior@outlook.com>
2021-04-11 11:27:29 -0300

    Adding Polimorfismo_Heranca project

---
+++ Polimorfismo_Heranca1/Entities/Employee.cs
@@ -0,0 +1,31 @@
+﻿namespace Polimorfismo_Heranca.Entities
+{
+    class Employee
+    {
+        //Attributes
+        public string Name { get; set; }
+        public int Hours { get; set; }
+
+        public double ValuPerHour { get; set; }
+
+        //Constructors
+        public Employee()
+        {
+        }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuPerHour = valuePerHour;
+        }
+
+        //Method
+        //"virtual" indica que esse método pode ser sobrescrito na subclasse!
+        public virtual double Payment()
+        {
+            return Hours * ValuPerHour;
+        }
+
+    }
+}
---
+++ Polimorfismo_Heranca1/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,26 @@
+﻿namespace Polimorfismo_Heranca.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge)
+            : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            /* O método "Payment" utilizado nessa subclasse
+             * é o próprio método da classe principal +
+             * 110% do valor de "AdditionalCharge".
+            */
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+    }
+}
