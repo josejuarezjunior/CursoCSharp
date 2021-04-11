0b1173630848e54ebca11815e2d21bc8225de9bc
Jose Juare <juarez.junior@outlook.com>
2021-04-11 00:27:47 -0300

    Adding Inheritance project

---
+++ Heranca/Entities/Account.cs
@@ -0,0 +1,34 @@
+﻿namespace Heranca.Entities
+{
+    class Account
+    {
+        //Attibutes
+        public int Number { get; private set; }//Número da conta
+        public string Holder { get; private set; }//Titular da conta
+        public double Balance { get; protected set; }//Saldo - O modificador de acesso "protected", indica que  esse dado somente será acessado na classe ou em sub-classes!
+
+        //Constructors
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        //Methods
+        public void Withdraw(double amount)//Saque
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)//Depósito
+        {
+            Balance += amount;
+        }
+
+    }
+}
---
+++ Heranca/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Heranca.Entities;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(1080, "Roberta", 100.00, 500.00);
+            Console.WriteLine("Balance: " + account.Balance);
+            Console.WriteLine("Account number: " + account.Number);
+            Console.WriteLine("Holder: " + account.Holder);
+
+            /*A operação abaixo não é possível, pois a alteração do saldo não está
+            ** acessível fora da classe "Account" e sua sub-classe, "BusinessAccount".
+            **
+            **account.Balance = 650.00;
+            */
+        }
+    }
+}
