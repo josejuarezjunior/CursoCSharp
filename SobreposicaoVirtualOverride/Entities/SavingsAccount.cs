1442e2d099842fbefc90d81b9573510f01c53525
Jose Juare <juarez.junior@outlook.com>
2021-04-11 11:01:37 -0300

    Adding SobreposicaoVirtualOverride project

---
+++ SobreposicaoVirtualOverride/Entities/SavingsAccount.cs
@@ -0,0 +1,29 @@
+﻿namespace SobreposicaoVirtualOverride.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        /*A palavra "override" aqui, indica que o método "Withdraw" da super-classe
+        **está sendo sobrescrevido aqui. Ou seja, o método terá comportamento diferente.
+        */
+        public override void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
---
+++ SobreposicaoVirtualOverride/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using SobreposicaoVirtualOverride.Entities;
+
+namespace SobreposicaoVirtualOverride
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(1001, "Alex", 500.0);
+            Account acc2 = new SavingsAccount(1002, "Anna", 500.0, 0.01);
+
+            acc1.Withdraw(10.0);
+            acc2.Withdraw(10.0);
+
+            Console.WriteLine("Saque da classe Account, com taxa de $5.00: $" + acc1.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Saque da classe SavingsAccount, sem taxa: $" + acc2.Balance.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
