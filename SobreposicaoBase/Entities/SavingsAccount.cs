fe8a1091d72c72d30b4bdf85f2c668f417e2da9a
Jose Juare <juarez.junior@outlook.com>
2021-04-11 11:09:45 -0300

    Adding SobreposicaoBase project

---
+++ SobreposicaoBase/Entities/SavingsAccount.cs
@@ -0,0 +1,35 @@
+﻿namespace SobreposicaoBase.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        /*A palavra "override" aqui, indica que o método da subclasse "Withdraw"
+        **está sendo sobrescrevido aqui. Ou seja, o método será diferente.
+        */
+        public override void Withdraw(double amount)
+        {
+            /*Nesse caso, o uso da palavra "base", utiliza o método da superclasse
+            ** e ainda é possível implementar mais operação.
+            **Portanto aqui será descontado os $5.00 da superclasse(Account)
+            **e mais $2.00. Totalizando assim $7.0.
+            */
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
---
+++ SobreposicaoBase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using SobreposicaoBase.Entities;
+
+namespace SobreposicaoBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(1001, "Alex", 500.0);
+            Account acc2 = new SavingsAccount(1002, "Anna", 500.0, 0.01);
+
+            acc1.Withdraw(10.0);
+            acc2.Withdraw(10.0);
+
+            Console.WriteLine("Saque com taxa da classe Account no valor de $5.00: $" + acc1.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Saque com taxa da classe Account no valor de $5.00 + $2.00 da classe SavingsAccount: $" + acc2.Balance.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
