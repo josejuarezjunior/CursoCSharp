a4f26f42c85e5772496a873d401f9949f050303a
Jose Juarez <juarez.junior@outlook.com>
2021-04-13 21:45:14 -0300

    Addin File Info and File Stream projects

---
+++ File_FileInfo_IOException/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+//Biblioteca necessária para trabalhar com File, FileInfo
+using System.IO;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             * "@" significa que a string é um caminho para um arquivo.
+             * Sem a "@", seria necessário usar barra dupla, ex:
+             * "c:\\temp\\file1.txt"
+            **/
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                //"CopyTo" irá copiar o conteúdo para o arquivo designado
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
