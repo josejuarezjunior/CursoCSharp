b36c1abd8ca44422410ff3474fa78d8f4199cef9
Jose Juarez <juarez.junior@outlook.com>
2021-04-14 21:43:49 -0300

    Adding TratamentoExcessao project

---
+++ TratamentoExcessao/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TratamentoExcessao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tratamento excessões , simples exemplo
+            Console.WriteLine("##### Division of two numbers ######");
+
+            bool correto = false;
+            do
+            {
+                try
+                {
+                    Console.Write("Type a number: ");
+                    double n1 = int.Parse(Console.ReadLine());
+                    Console.Write("Type another number: ");
+                    double n2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("--------------------------------------");
+                    double division = n1 / n2;
+                    correto = true;
+                    Console.WriteLine($"The division of {n1} by {n2} is {division}.");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("You must type a number!");
+                }
+            }
+            while (!correto);
+
+        }
+    }
+}
