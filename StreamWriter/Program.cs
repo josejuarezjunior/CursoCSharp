3916414a92b5ec5bb2df3c0afdeccc18169d606a
Jose Juarez <juarez.junior@outlook.com>
2021-04-14 21:29:53 -0300

    Adding projects

---
+++ StreamWriter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+namespace StreamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+            try
+            {
+                // As linhas do arquivo de origem serão lidas
+                string[] lines = File.ReadAllLines(sourcePath);
+                //As linhas serão adcionadas no arquivo de destino
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        //Serão escritas em letras maiúsculas
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (
+          IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
