6d757c9d1e3daea581a6f365340e6d6cf3e75d5e
Jose Juarez <juarez.junior@outlook.com>
2021-04-24 10:23:56 -0300

    Adding Interfaces3 project

---
+++ Interfaces3/Devices/ComboDevice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Interfaces3.Devices
+{
+
+    /*
+     * Essa classe implementa a classe abstrata "Device"
+     * e realiza as interfaces "IScanner" e "IPrinter":
+     */
+    class ComboDevice : Device, IScanner, IPrinter
+    {
+        /*
+         * O método "Print" é a realização do contrato com
+         * a interface "IPrinter"
+         */
+        public void Print(string document)
+        {
+            Console.WriteLine("Combodevice print " + document);
+        }
+        /*
+         * O método "ProcessDoc" é a implementação da super classe
+         * "Device", sendo sobrescrito (override).
+         */
+        public override void ProcessDoc(string document)
+        {
+            Console.WriteLine("Combodevice processing " + document);
+        }
+
+        /*
+         * O método "Scan" é a realização do contrato com
+         * a interface "IScanner"
+         */
+
+        public string Scan()
+        {
+            return "Combodevice scan result";
+        }
+    }
+}
---
+++ Interfaces3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Interfaces3.Devices;
+
+namespace Interfaces3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Object type Printer: ");
+            Printer p = new Printer() { SerialNumber = 1080 };
+            p.ProcessDoc("My letter");
+            p.Print("My letter");
+            Console.WriteLine();
+
+            Console.WriteLine("Object type Scanner: ");
+            Scanner s = new Scanner() { SerialNumber = 2003 };
+            s.ProcessDoc("My Email");
+            Console.WriteLine(s.Scan());
+            Console.WriteLine();
+
+            Console.WriteLine("Object type ComboDevice: ");
+            ComboDevice c = new ComboDevice() { SerialNumber = 3921 };
+            c.ProcessDoc("My dissertation");
+            c.Print("My dissertation");
+            Console.WriteLine(c.Scan());
+        }
+    }
+}
