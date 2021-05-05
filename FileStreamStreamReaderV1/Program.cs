73051dc6acd9be5efb579998d9299bb9fc29ce5b
Jose Juarez <juarez.junior@outlook.com>
2021-05-04 21:44:06 -0300

    Adding comments to FileStreamStreamReader project

--- FileStreamStreamReaderV1/Program.cs
+++ FileStreamStreamReaderV1/Program.cs
@@ -7,13 +7,27 @@ namespace Course
         static void Main(string[] args)
         {
             //Aula 187
+            //Caminho do arquivo externo
             string path = @"c:\temp\file1.txt";
+            /*
+             * FileStream nos permite fazer entrada e saída de dados em um arquivo externo.
+             * FileStream é uma steram binária
+            */
             FileStream fs = null;
+            /*
+             * StremReader é um objeto instanciado à partir do FileStream.
+             * É uma stream (sequência de dados) de caracteres.
+            */
             StreamReader sr = null;
             try
             {
+                /*
+                 * Primeira mente é instanciado um FileStream, associado à um arquivo externo
+                 * depois é instanciado um StreamReader associado à ao FileStream.
+                */
                 fs = new FileStream(path, FileMode.Open); // File.OpenRead(path);
                 sr = new StreamReader(fs);
+                //Associando uma linha do arquivo à uma string
                 string line = sr.ReadLine();
                 Console.WriteLine(line);
             }
@@ -24,7 +38,14 @@ namespace Course
             }
             finally
             {
+                /*
+                 * Como o recurso aberto(o arquivo externo) não é gerenciado
+                 * pelo CLR do .NET, é necessário fechar esse arquivo manualmente!!!
+                 * Abaixo o procedimento para fechar os recursos.
+                */
+                //Se o StreaReader é diferente de nulo, chamar o método Close para ele.
                 if (sr != null) sr.Close();
+                //Se o FileStream é diferente de nulo, chamar o método Close para ele.
                 if (fs != null) fs.Close();
             }
         }
