5473ea713e59bb182b42ee3c38398a533b66dddb
Jose Juarez <juarez.junior@outlook.com>
2021-05-18 21:22:13 -0300

    Adding New projects and improve InterfaceIComparable project

---
+++ GetHashCode_E_Equals/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GetHashCode_E_Equals.Entities;
+
+namespace GetHashCode_E_Equals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Adcionar mais comentários!!!!
+            Client a = new Client { Name = "Maria", Email = "Maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "Alex@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
