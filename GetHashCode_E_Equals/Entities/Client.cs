491c3e2c092b10dc39573c791e92479f13d383cc
Jose Juarez <juarez.junior@outlook.com>
2021-05-19 21:23:49 -0300

    Update GetHashCode_E_Equals project

--- GetHashCode_E_Equals/Entities/Client.cs
+++ GetHashCode_E_Equals/Entities/Client.cs
@@ -11,13 +11,21 @@ namespace GetHashCode_E_Equals.Entities
         public string Name { get; set; }
         public string Email { get; set; }
 
+        
+
         public override bool Equals(object obj)
         {
             if(!(obj is Client))
             {
                 return false;
             }
-            //Assistir aula de downcastig!
+            /*
+             * Downcastig (obj as Client) é necessário pois deve 
+             * respeitar a assinatura do método!
+             * O método Equals possui o parâmetro tipo object na assinatura. 
+             * Para que sua implementação do Equals seja considerada pela linguagem, 
+             * tem que respeitar essa assinatura.
+            */
             Client other = obj as Client;
             return Email.Equals(other.Email);
         }
--- GetHashCode_E_Equals/Program.cs
+++ GetHashCode_E_Equals/Program.cs
@@ -8,9 +8,28 @@ namespace GetHashCode_E_Equals
         static void Main(string[] args)
         {
 
-            //Adcionar mais comentários!!!!
+            
+            
             Client a = new Client { Name = "Maria", Email = "Maria@gmail.com" };
             Client b = new Client { Name = "Alex", Email = "Alex@gmail.com" };
+
+            /*
+             * GetHashCode e Equals
+             * São operações da classe Object utilizadas para comparar se um objeto
+             * é igual a outro
+             * 
+             * Equals: lento, resposta 100%
+             * Método que compara se o objeto é igual a outro, retornando
+             * true ou false.
+             * 
+             * GetHashCode: rápido, porém resposta positiva não é 100%
+             * Método que retorna um número inteiro representando um código 
+             * gerado a partir das informações do objeto
+             * 
+             * Os tipos pré-definidos(int, double, string..etc) já possuem 
+             * implementação para essas operações. 
+             * Classes e structs personalizados precisam sobrepô-las.
+            */
 
             Console.WriteLine(a.Equals(b));
             Console.WriteLine(a.GetHashCode());
