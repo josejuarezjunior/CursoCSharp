69aab8d9155b4825993767bcb0015ab6fc6d0649
Jose Juarez <juarez.junior@outlook.com>
2021-06-16 19:59:26 -0300

    Update Lambda expression to delegate variable

--- Comparison/Program.cs
+++ Comparison/Program.cs
@@ -15,7 +15,7 @@ namespace Comparison
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
-            Comparison<Product> comp = CompareProducts;
+            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
 
             list.Sort(comp);
 
@@ -24,9 +24,6 @@ namespace Comparison
                 Console.WriteLine(p);
             }
         }
-        static int CompareProducts(Product p1, Product p2)
-        {
-            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-        }
+  
     }
 }
