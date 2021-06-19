f8607cb48f2fae1390bc16db77cd3e53a2dd964f
Jose Juarez <juarez.junior@outlook.com>
2021-06-18 21:34:18 -0300

    Adding delegate projects

---
+++ Delegates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Delegates.Services;
+
+namespace Delegates
+{
+    /*
+     * Esse é um delegate que é uma referência para uma função com dois valores
+     * double, que retorna um valor double
+     * Para o exemplo em questão, esse delegate funciona com as funções SUM e MAX,
+     * porém não funciona com a função SQUARE, pois a mesma possui apenas um
+     * argumento double.
+    */
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            // BinaryNumericOperation op = CalculationService.Sum;
+            BinaryNumericOperation op = CalculationService.Sum;
+            // double result = op(a, b);
+            double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Predicate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Predicate.Entities;
+
+namespace Predicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            Console.WriteLine("Before RemoveAll");
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+            /*
+             * Predicate é um delegate(Uma função anônima) que recebe um objeto
+             * e retorna um valor booleano
+            */
+            list.RemoveAll(p => p.Price >= 100.00);
+
+            Console.WriteLine();
+            Console.WriteLine("After RemoveAll");
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
