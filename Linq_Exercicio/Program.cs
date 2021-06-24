fe1f3089875832be6bb11f952fdf7638c08fa356
Jose Juarez <juarez.junior@outlook.com>
2021-06-24 10:54:35 -0300

    Adding Linq exercises

---
+++ Linq_Exercicio/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
+using System.Linq;
+
+namespace Linq_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+
+            /*
+             * Exemplo do arquivo CSV em "c:\temp\in.txt"
+             * 
+             * Tv,900.00
+             * Mouse,50.00
+             * Tablet,350.50
+             * HD Case,80.90
+             * Computer,850.00
+             * Monitor,290.00
+            */
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                    list.Add(new Product(name, price));
+                }
+            }
+
+            //Encontrando o valor médio dos preços dos produtos
+            //Comando DefaultIfEmpty é inserido caso a lista seja vazia.
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Average price = " + avg.ToString("F2",CultureInfo.InvariantCulture));
+
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ Linq_Exercicio2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Linq_Exercicio2.Entities;
+using System.Globalization;
+
+namespace Linq_Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Employee> list = new List<Employee>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fields = sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        string email = fields[1];
+                        double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+                        list.Add(new Employee(name, email, salary));
+                    }
+                }
+
+                Console.WriteLine("Enter salary: ");
+                double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.WriteLine($"Email of people whose salary is more than {value}: ");
+                var em = list.Where(e => e.Salary > value).Select(e => e.Email);
+                foreach (string email in em)
+                {
+                    Console.WriteLine(email);
+                }
+                Console.WriteLine();
+
+                var msum = list.Where(e => e.Name[0] == 'M').Sum(e => e.Salary).ToString("F2", CultureInfo.InvariantCulture);
+                Console.WriteLine($"Sum of salary of people whose name starts with 'M': {msum}");
+                Console.WriteLine();
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
