fe17e67cd2ebf93bb81aacd85b62600022bda86f
Jose Juarez <juarez.junior@outlook.com>
2021-06-24 09:42:04 -0300

    Adding Linq_Sql project

---
+++ Linq_Sql/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Linq_Sql.Entities
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
